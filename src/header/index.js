9c25930908508a23b7ec486d36d5446c6866c988
Sabrina Aparecida S. Martins <sabi.sm12@gmail.com>
2023-06-20 11:54:35 -0300

    iniciando o projeto

---
+++ src/header/index.js
@@ -0,0 +1,16 @@
+
+import './header.css'
+
+export default function header() {
+    const title = "GLAZ"
+
+    return (
+        <div className='id-glaz'>
+
+            <h1>{title}</h1>
+        </div>
+    )
+
+}
+
+
